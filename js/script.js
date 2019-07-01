47e6d5aeab1bd26bdd98a65a779790b8656c5aba
jab0001 <48380304+jab0001@users.noreply.github.com>
2019-07-01 14:33:08 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var ButtonDelivery = document.querySelector(".delivery-button"),
+    ButtonWarranty = document.querySelector(".warranty-button"),
+    ButtonCredit = document.querySelector(".credit-button"),
+    DeliveryInfo = document.querySelector(".delivery-info"),
+    WarrantyInfo = document.querySelector(".warranty-info"),
+    CreditInfo = document.querySelector(".credit-info");
+
+
+ButtonDelivery.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        DeliveryInfo.classList.add(".info-show");
+});
+
+ButtonWarranty.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        WarrantyInfo.classList.add(".info-show");
+});
+
+ButtonCredit.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        CreditInfo.classList.add(".info-show");
+});
